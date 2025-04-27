05a43ccd9193f463bb7683f7a3c8809912682f8b
bhuwaneshtrivedi <bhuwanesht@birlasoft.com>
2025-04-27 17:23:14 +0530

    db error issue

--- src/main/java/com/javatechie/crud/example/entity/Product.java
+++ src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -2,19 +2,13 @@ package com.javatechie.crud.example.entity;
 
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 
 
-@Entity
-@Table(name = "PRODUCT_TBL")
+
 public class Product {
 
-    @Id
-    @GeneratedValue
+ 
     private int id;
     private String name;
     private int quantity;
